906b95617b9dec5b8005e1cf2379fc9e633520cb
Kishore <73517199+ikish0re@users.noreply.github.com>
2024-07-01 21:08:04 +0530

    Update legacy.sql

--- [SQL]/legacy.sql
+++ [SQL]/legacy.sql
@@ -995,7 +995,7 @@ INSERT INTO `jobs` (name, label) VALUES
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
 	('banker',0,'advisor','Consultant',10,'{}','{}'),
 	('banker',1,'banker','Banker',20,'{}','{}'),
-	('banker',2,'business_banker',"Investment banker",30,'{}','{}'),
+	('banker',2,'business_banker','Investment banker',30,'{}','{}'),
 	('banker',3,'trader','Broker',40,'{}','{}'),
 	('banker',4,'boss','Boss',0,'{}','{}')
 ;
